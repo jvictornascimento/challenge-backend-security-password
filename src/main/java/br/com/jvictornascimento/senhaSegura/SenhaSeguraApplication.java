52eceaa4b863ed0a29b36751f96920086d8dba2d
jvictornascimento <victojoao789@gmail.com>
2024-07-12 00:18:06 -0300

    init challenge

---
+++ src/main/java/br/com/jvictornascimento/senhaSegura/SenhaSeguraApplication.java
@@ -0,0 +1,13 @@
+package br.com.jvictornascimento.senhaSegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenhaSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenhaSeguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jvictornascimento/senhaSegura/SenhaSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jvictornascimento.senhaSegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenhaSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
